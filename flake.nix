7fbe97e593b0b4bdbd1ed7f1b3de75b0dbb070b3
Konrad Malik <konrad.malik@gmail.com>
2024-10-13 20:29:58 +0200

    flake.lock: Update
    Flake lock file updates:
    
    • Updated input 'nixpkgs':
        'github:nixos/nixpkgs/658e7223191d2598641d50ee4e898126768fe847?narHash=sha256-zACxiQx8knB3F8%2BZe%2B1BpiYrI%2BCbhxyWpcSID9kVhkQ%3D' (2024-09-17)
      → 'github:nixos/nixpkgs/b69de56fac8c2b6f8fd27f2eca01dcda8e0a4221?narHash=sha256-f0SBJz1eZ2yOuKUr5CA9BHULGXVSn6miBuUWdTyhUhU%3D' (2024-10-10)

--- flake.nix
+++ flake.nix
@@ -1,35 +1,65 @@
 {
   description = "incomplete.nvim";
 
+  nixConfig = {
+    extra-substituters = "https://neorocks.cachix.org";
+    extra-trusted-public-keys = "neorocks.cachix.org-1:WqMESxmVTOJX7qoBC54TwrMMoVI1xAM+7yFin8NRfwk=";
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    neorocks.url = "github:nvim-neorocks/neorocks";
+    gen-luarc = {
+      url = "github:mrcjkb/nix-gen-luarc-json";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { nixpkgs, ... }:
-    let
-      forAllSystems =
-        function:
-        nixpkgs.lib.genAttrs [
-          "x86_64-linux"
-          "aarch64-linux"
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ] (system: function nixpkgs.legacyPackages.${system});
-    in
     {
-      devShells = forAllSystems (pkgs: {
-        default = pkgs.mkShell {
-          name = "incomplete-shell";
-
-          packages = with pkgs; [
-            stylua
-            lua.pkgs.luacheck
-          ];
-
+      nixpkgs,
+      flake-parts,
+      neorocks,
+      gen-luarc,
+      ...
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              neorocks.overlays.default
+              gen-luarc.overlays.default
+            ];
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            shellHook =
+              let
+                luarc = pkgs.mk-luarc-json { };
+              in
+              # bash
+              ''
+                ln -fs ${luarc} .luarc.json
+              '';
+            packages = with pkgs; [
+              gnumake
+              busted-nlua
+              luajitPackages.luacheck
+              stylua
+            ];
+          };
+          formatter = pkgs.nixfmt-rfc-style;
         };
-      });
-      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
     };
-
 }
