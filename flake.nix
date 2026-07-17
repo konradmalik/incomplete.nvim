50efc5fdc69f72b8dbe3e928e9d3c10d771349d4
Konrad Malik <konrad.malik@gmail.com>
2026-07-17 08:59:47 +0200

    fix: drop x86_64-darwin

--- flake.nix
+++ flake.nix
@@ -30,7 +30,6 @@
         nixpkgs.lib.genAttrs [
           "x86_64-linux"
           "aarch64-linux"
-          "x86_64-darwin"
           "aarch64-darwin"
         ] (system: function (nixpkgsFor system));
     in
